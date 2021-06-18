2dec37973b7ce6eb8c616a33273c5f89d5d3dedb
bagbyj <bagbyj@wesclin.org>
2021-06-17 20:39:59 -0500

    Done up to Step 3.5: Engaging the JobData "model" in the transaction of information.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -17,12 +18,23 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "") //default GET mapping
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    ArrayList<Job> jobs = new ArrayList();
+    @PostMapping(value = "")
+    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        model.addAttribute(searchType, "searchType");
+        model.addAttribute(searchTerm, "searchTerm");
+        jobs=JobData.findAll();
+        if (searchTerm.toLowerCase().equals("all") || searchTerm == null){
+            jobs=JobData.findAll();
+        }
+        return "results";
+    }
 
 }
